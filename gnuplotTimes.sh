57d03e420d6b2b57ecde3f069de7baafb2417918
Joe Dunton <duntonjoe@gmail.com>
2019-08-15 20:23:20 -0400

    added today's data, added microtics to Y axis, improved Y range fit

--- gnuplotTimes.sh
+++ gnuplotTimes.sh
@@ -3,9 +3,10 @@ set title "Mile Time Progress"
 set xdata time
 set timefmt "%m/%d/%Y"
 set format x "%m/%d"
-set yrange ["5.00":"15.00"]
+set yrange ["10.00":"15.00"]
 set xlabel "Days"
 set ylabel "Pace (minutes)"
-set grid
+set mytics 4
+set grid xtics mytics ytics
 set xtics 24*60*60
 plot 'mileTimes.dat' using 1:2 t "My Mile Times" with linespoints
