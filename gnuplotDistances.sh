67d3762c7732853571fbcded93edac3bc09b64a8
Joe Dunton <duntonjoe@gmail.com>
2019-08-20 22:18:19 -0400

    added a new graphing script for distances and consolidated data files

---
+++ gnuplotDistances.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/gnuplot -persist
+set title "Total Workout Distance"
+set xdata time
+set timefmt "%m/%d/%Y"
+set format x "%m/%d"
+set yrange ["0.00":"8.00"]
+set xlabel "Days"
+set ylabel "Distance (Miles)"
+set mytics 8
+set xtics 7*24*60*60
+set grid xtics mytics ytics
+f(x) = mean_r
+g(x) = mean_b
+fit f(x) 'fitnessData.dat' u 1:3 via mean_r
+fit g(x) 'fitnessData.dat' u 1:4 via mean_b
+set label 1 sprintf("AVG RUN DIST = %f",mean_r) at graph 0.05,0.95
+set label 2 sprintf("AVG BIKE DIST = %f",mean_b) at graph 0.05,0.90
+plot 'fitnessData.dat' using 1:3 t "Distance Ran" with linespoint, \
+	'fitnessData.dat' using 1:4 t "Distance Biked" with linespoint, \
+	'fitnessData.dat' using 1:($3+$4) t "Total Distance" with lines dt 4
--- gnuplotTimes.sh
+++ gnuplotTimes.sh
@@ -10,6 +10,6 @@ set mytics 4
 set xtics 7*24*60*60
 set grid xtics mytics ytics
 f(x) = mean_y
-fit f(x) 'mileTimes.dat' u 1:2 via mean_y
+fit f(x) 'fitnessData.dat' u 1:2 via mean_y
 set label 1 sprintf("AVG = %f",mean_y) at graph 0.4,0.15
-plot 'mileTimes.dat' using 1:2 t "My Mile Times" with linespoints
+plot 'fitnessData.dat' using 1:2 t "My Mile Times" with linespoints
